3ffe9501f097c88008c3a4fdda33b0080d6642c9
Kukuh <krahmadani1@gmail.com>
2026-01-01 13:52:03 +0700

    Hidden password pada json user

--- backend/app/database/schema/user.go
+++ backend/app/database/schema/user.go
@@ -5,10 +5,10 @@ import (
 )
 
 type User struct {
-	ID       uint64 `gorm:"primary_key;column:id"`
-	Name     string `gorm:"column:name;not null"`
-	Password string `gorm:"column:password;not null"`
-	Email    string `gorm:"column:email;unique;not null"`
+	ID       uint64 `gorm:"primary_key;column:id" json:"id"`
+	Name     string `gorm:"column:name;not null" json:"name"`
+	Password string `gorm:"column:password;not null" json:"-"`
+	Email    string `gorm:"column:email;unique;not null" json:"email"`
 	Base
 }
 
